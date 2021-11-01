b18ec10235ce2c15c033684c2fac65c4d591b2c1
harshalwankhade <59637078+harshalwankhade@users.noreply.github.com>
2021-11-01 10:38:29 +0530

    Initial commit

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ToDoAPI.Data;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")] // api/todo
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public TodoController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult GetItems()
+        {
+            var items = _context.Items.ToList();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItem(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(z => z.Id == id);
+
+            if (item == null)
+                return NotFound();
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItem(ItemData data)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Items.AddAsync(data);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetItem", new { data.Id }, data);
+            }
+
+            return new JsonResult("Somethign Went wrong") { StatusCode = 500 };
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, ItemData item)
+        {
+            if (id != item.Id)
+                return BadRequest();
+
+            var existItem = await _context.Items.FirstOrDefaultAsync(z => z.Id == id);
+
+            if (existItem == null)
+                return NotFound();
+
+            existItem.Title = item.Title;
+            existItem.Details = item.Details;
+            existItem.Completed = item.Completed;
+
+            await _context.SaveChangesAsync();
+
+            // Following up the REST standart on update we need to return NoContent
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var existItem = await _context.Items.FirstOrDefaultAsync(z => z.Id == id);
+
+            if (existItem == null)
+                return NotFound();
+
+            _context.Items.Remove(existItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(existItem);
+        }
+    }
+}
---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Data
+{
+    public class ApiDbContext : IdentityDbContext
+    {
+        public DbSet<ItemData> Items { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
